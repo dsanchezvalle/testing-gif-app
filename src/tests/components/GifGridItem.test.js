83df25df39b30f2c3a7136db4417846a77406c32
David Sanchez <davsanchez@sofi.org>
2021-12-20 19:22:42 -0300

    add props to GifGridItem test

--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -3,8 +3,11 @@ import {shallow} from 'enzyme';
 import {GifGridItem} from '../../components/GifGridItem'
 
 describe('GifGridItem tests', ()=>{
+    const title = 'Test title';
+    const url = 'https://localhost/test.jpg';
+    
     test('it should render GifGridItem properly', ()=>{
-        const wrapper = shallow(<GifGridItem />);
+        const wrapper = shallow(<GifGridItem title={title} url={url} />);
         expect(wrapper).toMatchSnapshot();
     });
 })
