b9be113f92223cfc767f36c1b8d813733273afbb
David Sanchez <davsanchez@sofi.org>
2021-12-20 17:28:35 -0300

    update api key

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -3,7 +3,7 @@
 
 export const getGifs = async( category ) => {
 
-    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI( category ) }&limit=10&api_key=A8xMXqzieIHmtO3BjGLAtf1daSSDAv8K`;
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI( category ) }&limit=10&api_key=PJIoXPZ6kGC515c9JVIiurxRBwYy5RJm`;
     const resp = await fetch( url );
     const { data } = await resp.json();
 
