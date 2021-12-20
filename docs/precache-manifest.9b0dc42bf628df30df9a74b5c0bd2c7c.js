c05829de62598827125173af780b306d1896eea1
David Sanchez <davsanchez@sofi.org>
2021-12-20 14:08:12 -0300

    docs added

---
+++ docs/precache-manifest.9b0dc42bf628df30df9a74b5c0bd2c7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8271c226d6b274e14017a0fa9b8be03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1da5a45da9bbc6987deb",
+    "url": "/static/css/main.a5127213.chunk.css"
+  },
+  {
+    "revision": "3ec50c8fead662706b17",
+    "url": "/static/js/2.637c77a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.637c77a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1da5a45da9bbc6987deb",
+    "url": "/static/js/main.66b5d987.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
